c4831844e2a13b7fb4c1859d096f3cf1694f9b28
Pengbin Zhang <pengbin.zhang@castlery.com>
2025-06-04 11:31:29 +0800

    use mysql on railway cloud database

--- app/config.py
+++ app/config.py
@@ -5,11 +5,16 @@ import os
 
 load_dotenv()
 class Settings(BaseSettings):
-    REDSHIFT_USER: str
-    REDSHIFT_PASS: str
-    REDSHIFT_HOST: str
-    REDSHIFT_PORT: int = 5439
-    REDSHIFT_DB: str
+    # REDSHIFT_USER: str
+    # REDSHIFT_PASS: str
+    # REDSHIFT_HOST: str
+    # REDSHIFT_PORT: int = 5439
+    # REDSHIFT_DB: str
+    mysql_user: str = os.getenv("MYSQL_USER")
+    mysql_pass: str = os.getenv("MYSQL_PASS")
+    mysql_host: str = os.getenv("MYSQL_HOST")
+    mysql_port: int = 3306
+    mysql_db: str = os.getenv("MYSQL_DB")
 
     class Config:
         env_file = ".env"
--- app/db.py
+++ app/db.py
@@ -1,15 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
+import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
+# DATABASE_URL = (
+#     f"redshift+redshift_connector://{settings.REDSHIFT_USER}:{settings.REDSHIFT_PASS}@"
+#     f"{settings.REDSHIFT_HOST}:{settings.REDSHIFT_PORT}/{settings.REDSHIFT_DB}"
+# )
+# engine = create_engine(DATABASE_URL)
 
-DATABASE_URL = (
-    f"redshift+redshift_connector://{settings.REDSHIFT_USER}:{settings.REDSHIFT_PASS}@"
-    f"{settings.REDSHIFT_HOST}:{settings.REDSHIFT_PORT}/{settings.REDSHIFT_DB}"
+# mysql_url = (
+#     f"mysql+pymysql://{settings.mysql_user}:{settings.mysql_pass}@{settings.mysql_host}:{settings.mysql_port}/"
+#     f"{settings.mysql_db}"
+# )
+mysql_url = (
+    f"mysql://root:TkRZtpXAQfVrnVvGZASsLQAmxdGKGoZC@centerbeam.proxy.rlwy.net:41908/railway"
 )
-
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(mysql_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # 依赖注入用
