78031f79c1e5323e5431e34f86a0f94bb993eb72
Pengbin Zhang <pengbin.zhang@castlery.com>
2025-06-04 15:42:55 +0800

    change db.py

--- app/db.py
+++ app/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.config import settings
+from app.config import get_settings
 import os
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -12,8 +12,8 @@ pymysql.install_as_MySQLdb()
 # engine = create_engine(DATABASE_URL)
 
 mysql_url = (
-    f"mysql+pymysql://{settings.MYSQL_USER}:{settings.MYSQL_PASS}@{settings.MYSQL_HOST}:{settings.MYSQL_PORT}/"
-    f"{settings.MYSQL_DB}"
+    f"mysql+pymysql://{get_settings.MYSQL_USER}:{get_settings.MYSQL_PASS}@{get_settings.MYSQL_HOST}:{get_settings.MYSQL_PORT}/"
+    f"{get_settings.MYSQL_DB}"
 )
 # mysql_url = (
 #     f"mysql://root:TkRZtpXAQfVrnVvGZASsLQAmxdGKGoZC@centerbeam.proxy.rlwy.net:41908/railway"
